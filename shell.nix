9406fd90ca05fa97a84a4f05223ad932d3b2ac81
Albert Goncalves <goncalves.albert@gmail.com>
2020-02-23 23:55:25 -0500

    swap in llvmPackages -- test on macos!

--- shell.nix
+++ shell.nix
@@ -1,7 +1,6 @@
 with import <nixpkgs> {};
 let
     shared = [
-        clang_9
         python3
         shellcheck
     ];
@@ -10,12 +9,12 @@ let
     '';
 in
 {
-    darwin = stdenvNoCC.mkDerivation {
+    darwin = llvmPackages_9.stdenv.mkDerivation {
         name = "_";
         buildInputs = shared;
         shellHook = hook;
     };
-    linux = stdenvNoCC.mkDerivation {
+    linux = llvmPackages_9.stdenv.mkDerivation {
         name = "_";
         buildInputs = [
             valgrind
