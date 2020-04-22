238c2e04ddc4a7063db3efbe3c8191885ff81ec0
albertgoncalves <goncalves.albert@gmail.com>
2020-04-21 20:50:12 -0400

    swap

--- src/exit.h
+++ src/exit.h
@@ -4,10 +4,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static const char* BOLD_PURPLE    = "\033[1;35m";
-static const char* BOLD           = "\033[1m";
-static const char* BOLD_UNDERLINE = "\033[1;4m";
-static const char* CLOSE          = "\033[0m";
+#define BOLD_PURPLE    "\033[1;35m"
+#define BOLD           "\033[1m"
+#define BOLD_UNDERLINE "\033[1;4m"
+#define CLOSE          "\033[0m"
 
 #define EXIT_IF(condition)                                  \
     if (condition) {                                        \
